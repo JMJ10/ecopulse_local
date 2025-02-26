274c14db322d412a64f4ed6115225958a9920fc1
JMJ10 <joelmjoe2004@gmail.com>
2025-02-26 23:42:16 +0530

    admin functions

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(authRouter);  
 app.use(wasteRouter);
+app.use(adminRouter);
+app.use(userRouter);
+app.use(carbonRouter);
 
 const DB = "mongodb+srv://JoelJMJ:Joel2004@ecopulse.9sho1.mongodb.net/?retryWrites=true&w=majority&appName=EcoPulse"
 
